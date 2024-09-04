6c008f265282a398f8927bdf34300068b7f8f323
yasin al hasan <ya705668@gmail.com>
2024-09-04 13:07:44 +0600

    add service section and service item on card

--- src/components/HomePage/Service.js
+++ src/components/HomePage/Service.js
@@ -1,9 +1,13 @@
 import React from "react";
 import SectionTitle from "../sectionTitle/SectionTitle";
 
-const Service = () => {
+import { services } from "../../lib/services";
+import ServiceCard from "../cards/ServiceCard";
+
+const Service = ({ service }) => {
+  // console.log(services);
   return (
-    <div className="mt-10">
+    <div className="mt-10 min-h-screen">
       <SectionTitle
         subHeading={"Service"}
         heading={"Our Service Area"}
@@ -11,6 +15,12 @@ const Service = () => {
           "the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. "
         }
       ></SectionTitle>
+
+      <div className="container mx-auto mt-12 grid  grid-cols-1 lg:grid-cols-3 gap-6">
+        {services.map((service) => (
+          <ServiceCard service={service} key={service._id}></ServiceCard>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/cards/ServiceCard.js
+++ src/components/cards/ServiceCard.js
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
-const ServiceCard = () => {
-  return <div></div>;
+const ServiceCard = ({ service }) => {
+  const { title, img, price } = service || {};
+  return (
+    <div>
+      <div className="card card-compact bg-base-100 w-96 mx-auto shadow-xl">
+        <figure>
+          <Image src={img} height={120} width={430} alt="title" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title"> {title} </h2>
+          <div className="card-actions justify-between items-center">
+            <h6 className="text-primary font-semibold">Price: ${price} </h6>
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>{" "}
+    </div>
+  );
 };
 
 export default ServiceCard;
